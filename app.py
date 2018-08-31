6e15011b7618184ced03afb91213504503ecc840
markmcgrath2 <peter.ryder2@gmail.com>
2018-08-31 17:27:34 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, request
+import urllib, json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("template.html")
+
+@app.route('/weather', methods = ["GET", "POST"])
+def getweather():
+    if requests.method == "POST":
+        url = 'http://dataservice.accuweather.com/forecasts/v1/daily/1day/210463?apikey=pc2TdWiYE3WoXW0xP339FGj8HWqXpeEc'
+        try:
+            response = requests.get(url)
+        except requests.ConnectionError:
+            return "Connection Error"
+        jresponse = response.text
+        data = json.loads(jresponse)
+        print(data)
+        a = "foo"
+        return render_template("template.html",  a=a)
+    else:
+        print("entered else loop")
+        render_template("template.html")
+
+    
+
+   
+    
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
+
---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+// Execute when the DOM is fully loaded
+$(document).ready(function() {
+    $.getJSON("/weather", function() {
+        console.log("hello, this is in JS file")
+});
+});
