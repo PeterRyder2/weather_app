64b5d028b5ef7e406e1994862802f43c2af09cde
markmcgrath2 <peter.ryder2@gmail.com>
2018-09-03 14:11:54 +0100

    added nav bar, css, Foxford iamge, a weather template html page which uses Jinja 2 to acquire the weather data from foxford and show it on the screen

--- app.py
+++ app.py
@@ -2,15 +2,23 @@
 # API key = 8aaae57f966915b85ed963bd8af3866f
 # COdes for openweather https://openweathermap.org/weather-conditions
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import urllib, json, requests
-JSON = {'coord': {'lon': -9.12, 'lat': 53.98}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02n'}], 'base': 'stations', 'main': {'temp': 281.15, 'pressure': 1023, 'humidity': 100, 'temp_min': 281.15, 'temp_max': 281.15}, 'visibility': 10000, 'wind': {'speed': 3.1, 'deg': 320}, 'clouds': {'all': 20}, 'dt': 1535923800, 'sys': {'type': 1, 'id': 5238, 'message': 0.0043, 'country': 'IE', 'sunrise': 1535867276, 'sunset': 1535916158}, 'id': 2964233, 'name': 'Foxford', 'cod': 200}
+
+
+# todays JSON output
+JSON = {'coord': {'lon': -9.12, 'lat': 53.98}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}], 
+'base': 'stations', 'main': {'temp': 285.15, 'pressure': 1027, 'humidity': 76, 'temp_min': 285.15, 'temp_max': 285.15}, 
+'visibility': 10000, 'wind': {'speed': 4.1, 'deg': 310}, 'clouds': {'all': 20}, 'dt': 1535967000,
+'sys': {'type': 1, 'id': 5238, 'message': 0.002, 'country': 'IE', 'sunrise': 1535953728, 'sunset': 1536002486},
+'id': 2964233, 'name': 'Foxford', 'cod': 200}
 
 app = Flask(__name__)
  
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template("template.html")
+    return render_template("layout.html")
 
 @app.route('/weather', methods = ["GET", "POST"])
 def getweather():
@@ -23,12 +31,10 @@ def getweather():
             data = json.loads(jresponse)
         except:
             raise ValueError("could not load json properly")
-      
-
-        return render_template("template.html", a=data['weather'][0]['icon'])
+        return render_template("weatherres.html", title='Weather Result', weather=data['weather'][0]['description'], icon=data['weather'][0]['icon'])
     else:
         print("entered else loop")
-        render_template("template.html")
+        render_template("layout.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
